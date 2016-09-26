c4ae1092bf1b3bee4d13016d24315a86669f1090
Charlestng <charles.tanguy2@gmail.com>
2016-09-26 08:12:16 -0500

    Update Exercise2.R

--- Exercise2.R
+++ Exercise2.R
@@ -32,14 +32,36 @@ for (i in 2:ncol(X)){
 }
 
 # let's write a function that returns the local gradient component for an index
+# inputs:
+#    y, the target vector
+#    X, the explanatory variables
+#    beta, the parameters to be estimated
+#    m, the number or trials for binomial distribution
+#    index, the observation for which we calculate the contribution to the gradient
+# outputs:
+#    grad, the contribution to the gradient from the observation "index"
+
 gradient_local <- function(y, X, beta, m, index){
   grad <- - ((y[index] - m[index]) + m[index]*(1 - 1/(1 + exp(-X[index,]%*%beta))))*X[index,] 
   return(grad)
 }
 
 # let's write a function that return the gradient for a batch of indexes
+# inputs:
+#    y, the target vector
+#    X, the explanatory variables
+#    beta, the parameters to be estimated
+#    m, the number or trials for binomial distribution
+#    indexes, the list of observations for which we calculate the contribution to the gradient
+# outputs:
+#    grad, the contribution to the gradient from the observation "index"
+
 gradient_batch <- function(y, X, beta, m, indexes){
+  
+  # initialise the value of the partial gradient to 0
   grad <- 0
+  
+  # loop over the observations in the list
   for (index in indexes){
     grad <- grad + gradient_local(y, X, beta, m, index)
   }
@@ -47,6 +69,15 @@ gradient_batch <- function(y, X, beta, m, indexes){
 }
 
 # let's write a function that returns the log likelihood
+# log likelihood function
+# inputs:
+#    y, the target vector
+#    X, the explanatory variables
+#    beta, the parameters to be estimated
+#    m, the number or trials for binomial distribution
+# outputs:
+#    log_l, the log likelihood for the binomial distribution at point beta (scalar)
+
 log_likelihood <- function(y, X, beta, m){
   log_l <- 0
   for (i in 1:length(y)){
@@ -58,6 +89,15 @@ log_likelihood <- function(y, X, beta, m){
 
 # let's write a function that returns the individual contribution to the log likelihood
 # for an index
+# local log likelihood function
+# inputs:
+#    y, the target vector
+#    X, the explanatory variables
+#    beta, the parameters to be estimated
+#    m, the number or trials for binomial distribution
+#    index, the observation fo
+# outputs:
+#    log_l, the log likelihood for the binomial distribution at point beta (scalar)
 local_l_likelihood <- function(y, X, beta, m, index){
   L <- -lchoose(m[index],y[index]) - 
     (y[index] - m[index])*X[index,]%*%beta + 
@@ -75,7 +115,7 @@ batch_l_likelihood <- function(y, X, beta, m, indexes){
 }
 
 # The stochastic gradient descent function has the following characteristics
-#   The number of iteration for the algorithm is limited to 1000
+#   The number of iteration for the algorithm is limited to 10000
 #   The convergence is measured by the a criterion of growth rate for the exponential
 # moving average of the local contributions to the log likelihood
 #   At each iteration in the while loop a new index between 1 and n is picked at random in 
@@ -86,24 +126,36 @@ batch_l_likelihood <- function(y, X, beta, m, indexes){
 #      The final betas
 #      The list of log_likelihoods
 #      The number of iterations needed for convergence
-stochastic_gradient_descent <- function(y, X, beta, m, step, converged, decay){
+stochastic_gradient_descent <- function(y, X, beta, m, step, converged){
+  
+  # set the number of observations that can contribute
   sample_size <- length(y)
+  
+  # initialise the number of iterations
   iteration <- 0
+  
+  # initialise the convergence metric
   convergence <- 100
-  beta_list <- c(beta)
-  l_likelihood <- c(log_likelihood(y, X, beta, m)/sample_size)
+  
+  # initialise the list of betas
+  beta_list <- data.frame(t(beta))
+  
   while(iteration < 10000){ 
+    
+    # randomly pick an observation in the data set
     index <- sample(1:sample_size,1)
+    
+    # update beta according to the negative gradient direction
     beta <- beta - step * (iteration + 1)^(-5/9) * gradient_local(y, X, beta, m, index)
-    beta_list <- c(beta_list, beta)
+    
+    # update the list of betas
+    beta_list <- rbind(beta_list, data.frame(t(beta)))
+    
+    # update the number of iterations
     iteration <- iteration + 1
   }
   return(c("beta",
-         beta,
-         "list of loglikelihood",
-         l_likelihood,
-         "steps to convergence",
-         iteration))
+         beta))
 }
 
 fin <- stochastic_gradient_descent(y, X, beta, m, step=1, converged=0.000001, decay=0.1)
@@ -121,9 +173,7 @@ batch_gradient_descent <- function(y, X, beta, m, step, converged, decay, batch_
     iteration <- iteration + 1
   }
   return(c("beta",
-           beta,
-           "steps to convergence",
-           iteration))
+           beta))
 }
 
 test <- batch_gradient_descent(y, X, beta, m, step=0.1, converged=0.1, decay=0.1, batch_size=20)
