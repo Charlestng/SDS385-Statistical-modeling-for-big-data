fa76b9cf4a8b703e0eef25cd13f2369088c4caa6
Charlestng <charles.tanguy2@gmail.com>
2016-10-11 16:26:29 -0500

    Include CP stat and MOOSE

--- Exercise5.R
+++ Exercise5.R
@@ -51,3 +51,45 @@ for (i in 1:nrow(coefficients)){
   MSE <- rbind(MSE,
                cbind(coefficients[i,"lambda"],error))
 }
+
+MOOSE <- function(target, data, split_ratio, lambda){
+  n_obs <- nrow(data)
+  n_features <- ncol(data)
+  training_indexes <- sample(1:n_obs, floor(n_obs * split_ratio))
+  training_set <- data[training_indexes,]
+  training_target <- target[training_indexes,]
+  test_set <- data[!training_indexes,]
+  test_target <- target[!training_indexes,]
+  model <- glmnet(x = training_set, y = training_target, family = "gaussian")
+  coefficients <- t(as.matrix(coef(model, s = lambda)))
+  test_n_obs <- length(test_target)
+  error <- 1 / test_n_obs * crossprod(test_target - 
+                                      test_set %*% coefficients[2:n_features] - 
+                                      coefficients["(Intercept)"] * 
+                                      matrix(1, nrow = test_n_obs),
+                                      test_target - 
+                                      test_set %*% coefficients[2:n_features] - 
+                                      coefficients["(Intercept)"] * 
+                                      matrix(1, nrow = test_n_obs))
+  return(error)
+}
+
+Mallows_Cp <- function(target, data, lambda){
+  n_obs <- nrow(data)
+  n_features <- ncol(data)
+  model <- glmnet(x = data, y = target, family = "gaussian")
+  coefficients <- t(as.matrix(coef(model, s = lambda)))
+  MSE <- 1 / n_obs * crossprod(target -
+                                 data %*% coefficients[2:n_features] - 
+                                 coefficients["(Intercept)"] * 
+                                 matrix(1, nrow = n_obs),
+                                 target - 
+                                 data %*% coefficients[2:n_features] - 
+                                 coefficients["(Intercept)"] * 
+                                 matrix(1, nrow = n_obs))
+  S_lambda <- sum(coefficients != 0)
+  OLS_model <- lm(target ~ data)
+  sigma_hat_2 <- 1 / (n_obs - n_features) * sum(residuals(OLS_model)^2)
+  CP_stat <- MSE + 2 * S_lambda / n_obs * sigma_hat_2
+  return(CP_stat)
+}
